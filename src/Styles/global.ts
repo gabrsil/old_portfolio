4265b32c90f7ff0329f4d41b2f812e81248480f3
Gabriel <gabriel.aconsurgens@gmail.com>
2020-09-22 00:56:39 -0300

    make global styles

---
+++ src/Styles/global.ts
@@ -0,0 +1,24 @@
+import styled, {createGlobalStyle} from 'styled-components';
+
+export default createGlobalStyle`
+  
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body, html {
+        background-color: #f2f2f2;
+    }
+
+    :root {
+        --main-gray2: #4f4f4f;
+    }
+
+    ul { list-style-type: none; }
+    a { text-decoration: none; }
+
+
+
+`;
---
+++ src/Styles/globasl.ts
@@ -0,0 +1,32 @@
+import {createGlobalStyle} from 'styled-components';
+
+export default createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@300;700&display=swap');
+
+
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: 'Raleway', sans-serif;
+        box-sizing: border-box;
+        
+
+    }
+
+    body, html, #root {
+        max-height: 100vh;
+        max-width: 100vw;
+    
+    }
+
+    :root {
+        --bgprimary: #efedf2;
+        --main-gray2: #4F4F4F;
+    }
+
+
+
+
+
+`
