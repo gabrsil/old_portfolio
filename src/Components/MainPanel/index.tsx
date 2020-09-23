6c6edfc03aa12d84f408ba6b20b5f4268f0444ea
Gabriel <gabriel.aconsurgens@gmail.com>
2020-09-23 12:33:09 -0300

    add progress bar techs card

--- src/Components/MainPanel/index.tsx
+++ src/Components/MainPanel/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Container, SkillsArea, SkillsCard } from './styles'
 import InfoCard from '../InfoCard'
 import ProjectsInfo from '../ProjectsInfo'
+// import 'antd/dist/antd.css';
+
+
+import { Progress } from 'antd';
 
 const MainPanel: React.FC = () => {
   return (
@@ -15,14 +19,14 @@ const MainPanel: React.FC = () => {
 
                   <h1>Front end</h1>
                   <ul>
-                      <li>Html</li>
-                      <li>Css</li>
-                      <li>Javascript</li>
-                      <li>React</li>
-                      <li>Typescript</li>
-                      <li>Redux</li>
-                      <li>Styled-Components</li>
-                      <li>Sass</li>
+                      <li>Html <Progress percent={90} showInfo={false} /></li>
+                      <li>Css <Progress percent={90} showInfo={false} /></li>
+                      <li>Javascript <Progress percent={90} showInfo={false} /></li>
+                      <li>React <Progress percent={75} showInfo={false} /></li>
+                      <li>Typescript <Progress percent={60} showInfo={false} /></li>
+                      <li>Redux <Progress percent={60} showInfo={false} /></li>
+                      <li>Styled-Components <Progress percent={80} showInfo={false} /></li>
+                      <li>Sass <Progress percent={50} showInfo={false} /></li>
                   </ul>
                   </InfoCard>
                   
@@ -34,21 +38,21 @@ const MainPanel: React.FC = () => {
                  
                   <h1>Back end</h1>
                   <ul>
-                      <li>Nodejs</li>
-                      <li>Express</li>
-                      <li>Hapi</li>
-                      <li>Knex</li>
-                      <li>Mongoose</li>
-                      <li>Sequelize</li>
-                      <li>TypeOrm</li>
-                      <li>AdonisJS</li>
-                      <li>RabbitMQ</li>
-                      <li>Java Spring</li>
-                      <li>Mysql</li>
-                      <li>Postgres</li>
-                      <li>MongoDB</li>
-                      <li>Redis</li>
-                      <li>Acess</li>
+                      <li>Nodejs <Progress percent={85} showInfo={false} /></li>
+                      <li>Express <Progress percent={90} showInfo={false} /></li>
+                      <li>Hapi <Progress percent={60} showInfo={false} /></li>
+                      <li>Knex <Progress percent={80} showInfo={false} /></li>
+                      <li>Mongoose <Progress percent={80} showInfo={false} /></li>
+                      <li>Sequelize <Progress percent={50} showInfo={false} /></li>
+                      <li>TypeOrm <Progress percent={60} showInfo={false} /></li>
+                      <li>AdonisJS <Progress percent={50} showInfo={false} /></li>
+                      <li>RabbitMQ <Progress percent={50} showInfo={false} /></li>
+                      <li>Java Spring <Progress percent={60} showInfo={false} /></li>
+                      <li>Mysql <Progress percent={75} showInfo={false} /></li>
+                      <li>Postgres <Progress percent={80} showInfo={false} /></li>
+                      <li>MongoDB <Progress percent={80} showInfo={false} /></li>
+                      <li>Redis <Progress percent={60} showInfo={false} /></li>
+                      <li>Acess <Progress percent={75} showInfo={false} /></li>
                   </ul>
      
                   </InfoCard>
