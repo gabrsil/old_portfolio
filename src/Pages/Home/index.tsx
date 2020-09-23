fed12417f23ebf36b4618cf0c9d547bf3ba22861
Gabriel <gabriel.aconsurgens@gmail.com>
2020-09-23 12:11:45 -0300

    add footer to home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import SidePanel from '../../Components/SidePanel'
-import MainPanel from '../../Components/MainPanel'
+import SidePanel from '../../Components/SidePanel';
+import MainPanel from '../../Components/MainPanel';
+import Footer from '../../Components/Footer';
 import { Container } from './styles';
 
 
 const Home: React.FC = () => {
   return (
+    <>
     <Container>
-   
+      
       <SidePanel/>
       <MainPanel/>
     
 
     </Container>
+    <Footer/>
+    </>
   );
 }
 
