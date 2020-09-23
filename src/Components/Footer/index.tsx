714f2e7214326de3cf0636e9772dbc141e9f365a
Gabriel <gabriel.aconsurgens@gmail.com>
2020-09-23 12:28:11 -0300

    make footer component

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,10 +1,20 @@
 
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const Footer: React.FC = () => {
-  return <h1>lAl</h1>;
+  return (
+    <Container>
+      <div>
+
+        
+       <span>® 2020 - Gabriel</span>
+
+        
+      </div>
+    </Container>
+  );
 }
 
 export default Footer;
--- src/Components/Footer/styles.ts
+++ src/Components/Footer/styles.ts
@@ -1,5 +1,23 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  min-width: 100%;
+
+  background-color: #fff;
+  
+  div {
+   
+    display: flex;
+   
+    width: 280px;
+    margin: 0 auto;
+    justify-content: center;
+    text-align: center;
+
+    span {
+      font-size: 1.1rem;
+      color: #333333;
+    }
+  }
   
 `;
